7505d60a007842d3f178e3dd33026048853a1bca
Samantha Callie <Hoomee90@gmail.com>
2023-09-07 15:43:53 -0700

    change for each method call into a for loop

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,9 @@ window.addEventListener("load", function() {
   const iceCreamList = ["Mint Chip", "Cookies and Cream", "Cookie Dough", "Salted Caramel"];
   const list = document.querySelector("#favList");
   
-  iceCreamList.forEach(cream => {
+  for (let i = 0; i < iceCreamList.length; i++) {
     let listItem = document.createElement("li");
-    listItem.append(cream);
+    listItem.append(iceCreamList[i]);
     list.append(listItem);
-  });
+  };
 });
